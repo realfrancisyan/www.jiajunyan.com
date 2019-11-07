0fdfe033cc67cba2b9041e2a0e32e46bf574b3b3
Jiajun Yan <yanjiajun@nome.com>
2019-11-07 16:01:14 +0800

    项目初始化

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.scss';
+
+class Home extends React.Component {
+  render() {
+    return <div className={styles.title}>1233</div>;
+  }
+}
+
+export default Home;
