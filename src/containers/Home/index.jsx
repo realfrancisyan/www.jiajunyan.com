bb26cfac3d20455bc57f834b0aa821315478e380
Jiajun Yan <yanjiajun@nome.com>
2019-11-11 11:16:39 +0800

    添加登出功能

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,13 +2,58 @@ import React from 'react';
 import './index.scss';
 import Header from '../../components/Header';
 import Posts from '../../components/Posts';
+import bindAll from 'lodash.bindall';
+
+const LogOutButton = props => {
+  if (props.token) {
+    return (
+      <div className="logout">
+        <button onClick={props.handleLogOut}>登出</button>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ''
+    };
+    bindAll(this, ['handleLogOut']);
+    this.postsComponent = React.createRef();
+  }
+
+  handleGetToken() {
+    const token = localStorage.getItem('token');
+    this.setState({
+      token
+    });
+  }
+
+  handleLogOut() {
+    localStorage.removeItem('token');
+    this.postsComponent.handleCheckToken();
+    this.setState({
+      token: ''
+    });
+  }
+
+  componentDidMount() {
+    this.handleGetToken();
+  }
+
   render() {
     return (
       <div className="home-container">
         <Header></Header>
-        <Posts></Posts>
+        <Posts ref={cd => (this.postsComponent = cd)}></Posts>
+        <LogOutButton
+          handleLogOut={this.handleLogOut}
+          token={this.state.token}
+        ></LogOutButton>
       </div>
     );
   }
