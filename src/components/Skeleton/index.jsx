5492bd11aa5694373604361df2d8802d29ee4ab7
Jiajun Yan <yanjiajun@nome.com>
2019-11-15 23:19:50 +0800

    修复样式问题

--- src/components/Skeleton/index.jsx
+++ src/components/Skeleton/index.jsx
@@ -59,7 +59,7 @@ class Skeleton extends React.Component {
 
   render() {
     return (
-      <div className="home-container">
+      <div className="home-container box500 skeleton">
         <Loader
           innerWidth={this.state.innerWidth}
           skeletonSize={this.state.skeletonSize}
