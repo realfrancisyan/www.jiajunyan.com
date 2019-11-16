bcc309eaef7c8a697e4bc016848fd8033a5716e5
Jiajun Yan <yanjiajun@nome.com>
2019-11-16 21:20:46 +0800

    ud

---
+++ docs/build/precache-manifest.456c9fb111ca19d5d81e6ffd65b009f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7a06322b476b557b83cadeec5d849f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35bf4778a95c559750e9",
+    "url": "./static/css/main.1d5218f0.chunk.css"
+  },
+  {
+    "revision": "08513a645d10e6c6936b",
+    "url": "./static/js/2.3c27faf8.chunk.js"
+  },
+  {
+    "revision": "35bf4778a95c559750e9",
+    "url": "./static/js/main.c4d59f63.chunk.js"
+  },
+  {
+    "revision": "9e6c8b6707403d8eb7f0",
+    "url": "./static/js/runtime-main.8ed23765.js"
+  },
+  {
+    "revision": "86aa9d503765275e2bbedda11eb0d8c7",
+    "url": "./static/media/logo.86aa9d50.svg"
+  }
+]);
