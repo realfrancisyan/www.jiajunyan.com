18bba340f9f84e4d6b5b8f7345d6c935c3d5e8ec
Jiajun Yan <yanjiajun@nome.com>
2019-11-20 22:39:34 +0800

    调整骨架屏宽度

--- src/components/Skeleton/index.jsx
+++ src/components/Skeleton/index.jsx
@@ -13,15 +13,15 @@ const Loader = props => (
     primaryColor="#f3f3f3"
     secondaryColor="#ecebeb"
   >
-    <rect x="0" y="0" rx="3" ry="3" width="70" height="10" />
-    <rect x="80" y="0" rx="3" ry="3" width="100" height="10" />
-    <rect x="190" y="0" rx="3" ry="3" width="10" height="10" />
-    <rect x="15" y="20" rx="3" ry="3" width="130" height="10" />
-    <rect x="155" y="20" rx="3" ry="3" width="130" height="10" />
-    <rect x="15" y="40" rx="3" ry="3" width="90" height="10" />
-    <rect x="115" y="40" rx="3" ry="3" width="60" height="10" />
-    <rect x="185" y="40" rx="3" ry="3" width="60" height="10" />
-    <rect x="0" y="60" rx="3" ry="3" width="30" height="10" />
+    <rect x="0" y="0" rx="3" ry="3" width="90" height="10" />
+    <rect x="80" y="0" rx="3" ry="3" width="120" height="10" />
+    <rect x="190" y="0" rx="3" ry="3" width="30" height="10" />
+    <rect x="15" y="20" rx="3" ry="3" width="150" height="10" />
+    <rect x="155" y="20" rx="3" ry="3" width="150" height="10" />
+    <rect x="15" y="40" rx="3" ry="3" width="110" height="10" />
+    <rect x="115" y="40" rx="3" ry="3" width="80" height="10" />
+    <rect x="185" y="40" rx="3" ry="3" width="80" height="10" />
+    <rect x="0" y="60" rx="3" ry="3" width="50" height="10" />
   </ContentLoader>
 );
 
@@ -29,8 +29,8 @@ class Skeleton extends React.Component {
   state = {
     innerWidth: 0,
     skeletonSize: {
-      minWidth: 290, // 骨架屏移动端宽度
-      maxWidth: 290, // 骨架屏 PC 端宽度
+      minWidth: 310, // 骨架屏移动端宽度
+      maxWidth: 400, // 骨架屏 PC 端宽度
       height: 100 // 骨架屏高度
     }
   };
