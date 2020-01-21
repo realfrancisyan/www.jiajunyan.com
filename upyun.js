e2ada4f8770bb811259cbf0f9cd7a61361357339
Jiajun Yan <yanjiajun@nome.com>
2020-01-21 15:05:55 +0800

    更新 node-upyun 版本

--- upyun.js
+++ upyun.js
@@ -1,20 +1,10 @@
 const UpyunService = require('node-upyun-plugin');
 
-const config = {
-  name: 'upload-test-jiajun',
-  // name: 'jiajunyan',
-  operator: 'jiajun',
-  password: 'CSCn3zDDqUNQUl6wrDv1xCK00NlBro03'
-};
-
-const { name, operator, password } = config;
-const folderPath = 'build';
-
 const us = new UpyunService({
-  name,
-  operator,
-  password,
-  folderPath
+  name: 'jiajunyan',
+  operator: 'jiajun',
+  password: 'CSCn3zDDqUNQUl6wrDv1xCK00NlBro03',
+  folderPath: 'build'
 });
 
-us.upload();
+us.upload({ removeAll: true });
